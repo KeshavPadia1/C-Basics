558006dfe24713ab8b503dd1a4ae2e7bda7347f6
KeshavPadia1 <keshavpadia48@gmail.com>
2025-07-23 14:01:52 +0530

    while_loop.c

--- while_loop.c
+++ while_loop.c
@@ -8,3 +8,16 @@ int main() {
     }
     return 0;
 }
+
+// this code will also give same output 
+
+// #include <stdio.h>
+
+// int main() {
+//     int i =0;
+//     while(i<4){
+//         printf("hello world\n");
+//         i++;
+//     }
+//     return 0;
+// }
