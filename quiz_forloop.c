ca254a37bea856a374256d2889c2c1067074ac70
KeshavPadia1 <keshavpadia48@gmail.com>
2025-07-23 19:09:52 +0530

    quiz_forloop.c

--- quiz_forloop.c
+++ quiz_forloop.c
@@ -2,9 +2,9 @@
 
 int main() {
     int n;
-    printf("Enter the number n to which you want to print natural numbers\n");
+    printf("Enter the number n till which you want to print natural numbers\n");
     scanf("%d" , &n);
-    for (int i = 1; i < n; i++){
+    for (int i = 1; i <= n; i++){
         printf("%d\n" , i);
     }
     
